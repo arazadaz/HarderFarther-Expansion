1b2f10a34bf1026a2e9aa1ca802d9cf0b4f77503
arazadaz <30640837+arazadaz@users.noreply.github.com>
2023-05-12 22:58:06 -0500

    Delete remappedSrc directory

--- remappedSrc/com/mactso/harderfarther/network/SyncAddOneGCToClientPacket.java
+++
@@ -1,5 +0,0 @@
-package com.mactso.harderfarther.network;
-
-public class SyncAddOneGCToClientPacket {
-
-}
--- remappedSrc/com/mactso/harderfarther/timer/IChunkLastMobDeathTime.java
+++
@@ -1,9 +0,0 @@
-package com.mactso.harderfarther.timer;
-
-public interface IChunkLastMobDeathTime {
-
-
-		public long getLastKillTime();
-		public void setLastKillTime(long gametime);
-		
-}
