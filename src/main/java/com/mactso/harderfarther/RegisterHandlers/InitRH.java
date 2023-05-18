ee0323c9a1e5c4c912783735dc294e5d403f98ad
Arazadaz <Arazechaz@gmail.com>
2023-05-17 23:49:31 -0500

    Refactor mod item registering to fabric

--- src/main/java/com/mactso/harderfarther/RegisterHandlers/InitRH.java
+++ src/main/java/com/mactso/harderfarther/RegisterHandlers/InitRH.java
@@ -1,9 +1,13 @@
 package com.mactso.harderfarther.RegisterHandlers;
 
+import com.mactso.harderfarther.item.ModItems;
+
 public class InitRH {
 
 
     public static void registerAll(){
+
+        ModItems.register();
 
     }
 
--- src/main/java/com/mactso/harderfarther/item/ModItems.java
+++ src/main/java/com/mactso/harderfarther/item/ModItems.java
@@ -1,5 +1,6 @@
 package com.mactso.harderfarther.item;
 
+import com.mactso.harderfarther.Main;
 import com.mactso.harderfarther.block.ModBlocks;
 import com.mactso.harderfarther.utility.Utility;
 import net.minecraft.item.BlockItem;
@@ -7,7 +8,9 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
+import net.minecraft.util.registry.Registry;
 
 
 public class ModItems {
@@ -17,13 +20,12 @@ public class ModItems {
 	public static final Item DEAD_BRANCHES = new BlockItem(ModBlocks.DEAD_BRANCHES, (new Item.Settings()).group(ItemGroup.DECORATIONS));
 	public static final Item BURNISHING_STONE = new BurnishingStone(new Item.Settings().group(ItemGroup.MISC).fireproof().rarity(Rarity.RARE));
 	public static ItemStack BURNISHING_STONE_STACK = null;
-	
-	public static void register(IForgeRegistry<Item> forgeRegistry)
-	{
 
-		forgeRegistry.register("dead_branches", DEAD_BRANCHES);
-		forgeRegistry.register("life_heart", LIFE_HEART);
-		forgeRegistry.register("burnishing_stone",BURNISHING_STONE);
+
+	public static void register(){
+		Registry.register(Registry.ITEM, new Identifier(Main.MODID, "dead_branches"), DEAD_BRANCHES);
+		Registry.register(Registry.ITEM, new Identifier(Main.MODID, "life_heart"), LIFE_HEART);
+		Registry.register(Registry.ITEM, new Identifier(Main.MODID, "burnishing_stone"), BURNISHING_STONE);
 
 		LIFE_HEART_STACK = new ItemStack(LIFE_HEART, 1);
 		Utility.setLore(LIFE_HEART_STACK,
@@ -33,5 +35,6 @@ public class ModItems {
 		Utility.setLore(BURNISHING_STONE_STACK,
 				Text.Serializer.toJson(Text.translatable("item.harderfarther.burnishing_stone.lore")));
 	}
+
 
 }
