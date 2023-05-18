1bbb87d9b06996da536b1d8151e833092342f72e
Arazadaz <Arazechaz@gmail.com>
2023-05-17 19:57:22 -0500

    Config refactoring/fabric Register Handler Skeleton

--- src/main/java/com/mactso/harderfarther/Main.java
+++ src/main/java/com/mactso/harderfarther/Main.java
@@ -2,7 +2,9 @@
 package com.mactso.harderfarther;
 
 import java.lang.reflect.Field;
+import java.util.List;
 
+import net.minecraft.util.math.BlockPos;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
@@ -11,7 +13,7 @@ import org.jetbrains.annotations.Nullable;
 import com.mactso.harderfarther.block.ModBlocks;
 import com.mactso.harderfarther.blockentities.ModBlockEntities;
 import com.mactso.harderfarther.command.HarderFartherCommands;
-import com.mactso.harderfarther.config.MyConfig;
+import com.mactso.harderfarther.config.PrimaryConfig;
 import com.mactso.harderfarther.events.BlockEvents;
 import com.mactso.harderfarther.events.ChunkEvent;
 import com.mactso.harderfarther.events.ExperienceDropEventHandler;
@@ -25,6 +27,7 @@ import com.mactso.harderfarther.events.WorldTickHandler;
 import com.mactso.harderfarther.item.ModItems;
 import com.mactso.harderfarther.manager.GrimCitadelManager;
 import com.mactso.harderfarther.network.Register;
+import com.mactso.harderfarther.RegisterHandlers.InitRH;
 import com.mactso.harderfarther.sounds.ModSounds;
 import com.mactso.harderfarther.utility.LootHandler.HFLootModifier;
 import com.mactso.harderfarther.utility.Utility;
@@ -34,21 +37,82 @@ import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
 
 
-public class Main {
+public class Main implements ModInitializer {
 
-	    public static final String MODID = "harderfarther"; 
-		private static final Logger LOGGER = LogManager.getLogger();
-		private static final int MAX_USABLE_VALUE = 16000000;  // you can subtract 1 from this number.
+	public static final String MODID = "harderfarther";
+	private static final Logger LOGGER = LogManager.getLogger();
+	private static final int MAX_USABLE_VALUE = 16000000;  // you can subtract 1 from this number.
 
-	    
-	    public Main()
-	    {
 
-	    	Utility.debugMsg(0,MODID + ": Registering Mod.");
-	  		FMLJavaModLoadingContext.get().getModEventBus().register(this);
- 	        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON,MyConfig.COMMON_SPEC );
 
-	    }
+	//	Harder Farther Control Values"."Debug Settings
+	public static int debugLevel;
+
+	//	Harder Farther Control Values"."Farm Limiter Settings
+	public static int limitMobFarmsTimer;
+
+
+	//	Harder Farther Control Values"."HarderFarther Settings
+	public static boolean onlyOverworld;
+	public static boolean makeMonstersHarderFarther;
+	public static List<? extends String> dimensionOmitList;
+	public static int boostMaxDistance;
+	public static int boostMinDistance;
+	public static int safeDistance;
+	public static int minimalSafeAltitude;
+	public static int maximumSafeAltitude;
+
+
+	//	Harder Farther Control Values"."Loot Settings
+	public static boolean useLootDrops;
+	public static int oddsDropExperienceBottle;
+	public static List<? extends String> lootItemsList;
+
+
+	//	Harder Farther Control Values"."Boost Settings
+	public static int hpMaxBoost;
+	public static int speedBoost;
+	public static int atkDmgBoost;
+	public static int knockbackBoost;
+
+
+	//	Harder Farther Control Values"."Harder Over Time Settings
+	public static boolean makeHarderOverTime;
+	public static int maxHarderTimeMinutes;
+
+
+	//	Harder Farther Control Values"."Grim Citadel Settings
+	public static boolean useGrimCitadels;
+	public static List<BlockPos> grimCitadelsBlockPosList;
+
+	public static int grimCitadelsCount;
+	public static int grimCitadelsRaidus;
+	public static int grimCitadelBonusDistance;
+	public static int grimCitadelPlayerCurseDistance;
+	public static int grimCitadelMaxBoostPercent;
+
+
+	//	Harder Farther Control Values"."Grim Effects Settings
+	public static boolean grimEffectTrees;
+	public static boolean grimEffectAnimals;
+	public static boolean grimEffectPigs;
+	public static boolean grimEffectVillagers;
+	public static int grimLifeHeartPulseSeconds;
+
+
+
+	public void onInitialize() {
+		PrimaryConfig.initConfig();
+
+		if (Main.debugLevel > 0) {
+			System.out.println("Harder Farther Debug Level: " + Main.debugLevel );
+		}
+
+		InitRH.registerAll();
+	}
+
+
+
 
 		@OnlyIn(Dist.CLIENT)
 	    @SubscribeEvent
