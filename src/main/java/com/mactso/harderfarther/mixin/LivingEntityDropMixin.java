863a94a6441a686ad61ead580fef6bc17e2e2782
Arazadaz <Arazechaz@gmail.com>
2023-05-23 12:03:02 -0500

    Fix LivingEntityDropMixin class again. ;-;

--- src/main/java/com/mactso/harderfarther/mixin/LivingEntityDropMixin.java
+++ src/main/java/com/mactso/harderfarther/mixin/LivingEntityDropMixin.java
@@ -4,14 +4,16 @@ import com.mactso.harderfarther.events.LivingEntityDropCallback;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.util.ActionResult;
+import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+@Mixin(LivingEntity.class)
 public class LivingEntityDropMixin {
 
     @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;dropLoot(Lnet/minecraft/entity/damage/DamageSource;Z)V"), method = "drop", cancellable = true)
-    private void onDrop(final DamageSource source, final CallbackInfoReturnable<Boolean> info) {
+    private void onDrop(final DamageSource source, final CallbackInfo info) {
         ActionResult result = LivingEntityDropCallback.EVENT.invoker().interact(source, (LivingEntity) (Object) this);
 
         if(result == ActionResult.FAIL) {
