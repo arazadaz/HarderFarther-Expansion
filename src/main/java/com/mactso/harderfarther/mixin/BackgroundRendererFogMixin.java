052dab614605e44f1ab5eaeb405bb5b8952f74ab
Arazadaz <Arazechaz@gmail.com>
2023-05-27 14:54:00 -0500

    Fix Fog Events

--- src/main/java/com/mactso/harderfarther/mixin/BackgroundRendererFogMixin.java
+++ src/main/java/com/mactso/harderfarther/mixin/BackgroundRendererFogMixin.java
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(BackgroundRenderer.class)
 public class BackgroundRendererFogMixin {
 
-    @Inject(method = "applyFog", at = @At("HEAD"))
+    @Inject(method = "applyFog", at = @At("TAIL"))
     private static void onFog(Camera camera, BackgroundRenderer.FogType fogType, float viewDistance, boolean thickFog, float tickDelta, CallbackInfo info) {
         ActionResult result = FogRenderCallback.EVENT.invoker().interact(camera, fogType, viewDistance, thickFog, tickDelta);
 
--- src/main/java/com/mactso/harderfarther/mixin/RenderSystemFogColorMixin.java
+++ src/main/java/com/mactso/harderfarther/mixin/RenderSystemFogColorMixin.java
@@ -3,14 +3,18 @@ package com.mactso.harderfarther.mixin;
 import com.mactso.harderfarther.events.FogColorCallback;
 import com.mojang.blaze3d.systems.RenderSystem;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(RenderSystem.class)
-public class RenderSystemFogColorMixin {
+public abstract class RenderSystemFogColorMixin {
 
-    @Inject(method = "setShaderFogColor", at = @At("HEAD"), remap = false)
+    @Shadow(remap = false)
+    static void _setShaderFogColor(float f, float g, float h, float i){};
+
+    @Inject(method = "setShaderFogColor", at = @At("TAIL"), remap = false)
     private static void onFog(float f, float g, float h, float i, CallbackInfo info) {
         float[] newColor = FogColorCallback.EVENT.invoker().interact(f, g, h, i);
 
@@ -18,6 +22,7 @@ public class RenderSystemFogColorMixin {
         g = newColor[1];
         h = newColor[2];
         i = newColor[3];
+        _setShaderFogColor(f, g, h, i);
     }
 
 }
