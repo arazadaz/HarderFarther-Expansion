9155a6599c9d59b64130503361cb2a418826e322
Arazadaz <Arazechaz@gmail.com>
2023-06-05 05:17:33 -0500

    StructureMixin logic for handling spawn.

--- src/main/java/com/mactso/harderfarther/mixin/StructureStartMixin.java
+++ src/main/java/com/mactso/harderfarther/mixin/StructureStartMixin.java
@@ -12,6 +12,7 @@ import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.random.RandomGenerator;
 import net.minecraft.world.StructureWorldAccess;
+import net.minecraft.world.World;
 import net.minecraft.world.biome.source.BiomeCoords;
 import net.minecraft.world.biome.source.BiomeSource;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
@@ -32,11 +33,11 @@ public class StructureStartMixin {
     private ArrayList<List<String>> difficultySectionStructure = new ArrayList<>();
 
     @Inject(at = @At(value = "HEAD"), method = "Lnet/minecraft/structure/StructureStart;placeInChunk(Lnet/minecraft/world/StructureWorldAccess;Lnet/minecraft/structure/StructureManager;Lnet/minecraft/world/gen/chunk/ChunkGenerator;Lnet/minecraft/util/random/RandomGenerator;Lnet/minecraft/util/math/BlockBox;Lnet/minecraft/util/math/ChunkPos;)V", cancellable = true)
-    private void onPlace(StructureWorldAccess world, StructureManager structureManager, ChunkGenerator chunkGenerator, RandomGenerator random, BlockBox boundingBox, ChunkPos chunkPos, CallbackInfo ci){
+    private void onPlace(StructureWorldAccess world, StructureManager structureManager, ChunkGenerator chunkGenerator, RandomGenerator random, BlockBox boundingBox, ChunkPos chunkPos, CallbackInfo ci) {
 
-        if(!areListInitialized){
+        if (!areListInitialized) {
 
-            OreConfig.getDifficultySections().forEach(section ->{
+            OreConfig.getDifficultySections().forEach(section -> {
                 difficultySectionNumbers.add(section.first);
                 difficultySectionStructure.add(section.second);
             });
@@ -50,40 +51,72 @@ public class StructureStartMixin {
 
 
 
-        int x = BiomeCoords.fromChunk(chunkPos.getCenterX());
-        int z = BiomeCoords.fromChunk(chunkPos.getCenterZ());
-
-        String structure = ((IExtendedChunkRegion)world).getGeneratingStructureResourceKey();
-        structure = structure.substring(0, structure.length()-1);
-        structure = structure.split("/")[2].substring(1);
-
-        System.out.println(structure);
-
-        BiomeSource biomeSource = ((IExtendedChunkRegion)world).getBiomeSource();
 
 
-        if(((IExtendedBiomeSourceHF)biomeSource).getInit()) {
+
+
+
+        BiomeSource biomeSource = ((IExtendedChunkRegion) world).getBiomeSource();
+
+        if (((IExtendedBiomeSourceHF) biomeSource).getInit()) {
 
             ServerWorld worldReal = ((IExtendedBiomeSourceHF) biomeSource).getDirtyWorld();
 
+            int x = BiomeCoords.fromChunk(chunkPos.getCenterX());
+            int z = BiomeCoords.fromChunk(chunkPos.getCenterZ());
 
-            float difficulty = HarderFartherManager.getDistanceDifficultyHere(worldReal, new Vec3d(x, 0, z)) * 100;
+            String structure = ((IExtendedChunkRegion) world).getGeneratingStructureResourceKey();
+            structure = structure.substring(0, structure.length() - 1);
+            structure = structure.split("/")[2].substring(1);
 
-            int[] choosenAreaIndex = {-1};
-            difficultySectionNumbers.forEach(difficultySectionNumber -> {
-                if (difficulty >= difficultySectionNumber) choosenAreaIndex[0]++;
-            });
+            //System.out.println(structure);
 
 
-            if (difficultySectionStructure.get(choosenAreaIndex[0]).get(0).equals("")) {
+            if (worldReal.getRegistryKey() == World.OVERWORLD) {
+
+
+                float difficulty = HarderFartherManager.getDistanceDifficultyHere(worldReal, new Vec3d(x, 0, z)) * 100;
+
+                int[] choosenAreaIndex = {-1};
+                difficultySectionNumbers.forEach(difficultySectionNumber -> {
+                    if (difficulty >= difficultySectionNumber) choosenAreaIndex[0]++;
+                });
+
+
+                if (difficultySectionStructure.get(choosenAreaIndex[0]).get(0).equals("")) {
+                    return;
+                }
+
+                if (!difficultySectionStructure.get(choosenAreaIndex[0]).contains(structure)) {
+                    ci.cancel();
+                }
+            }
+
+        }
+
+
+
+        //repeat logic for spawn before worlds are initialized.
+        if(((IExtendedBiomeSourceHF) biomeSource).getInit()){
+
+            int x = BiomeCoords.fromChunk(chunkPos.getCenterX());
+            int z = BiomeCoords.fromChunk(chunkPos.getCenterZ());
+
+            String structure = ((IExtendedChunkRegion) world).getGeneratingStructureResourceKey();
+            structure = structure.substring(0, structure.length() - 1);
+            structure = structure.split("/")[2].substring(1);
+
+
+
+            if (difficultySectionStructure.get(0).get(0).equals("")) {
                 return;
             }
 
-            if (!difficultySectionStructure.get(choosenAreaIndex[0]).contains(structure)) {
+            if (!difficultySectionStructure.get(0).contains(structure)) {
                 ci.cancel();
             }
-        }
 
+        }
     }
 
 }
