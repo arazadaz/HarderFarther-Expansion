ee0aa23a39a9d368184cc2ea9158f861d999bc64
Arazadaz <Arazechaz@gmail.com>
2023-06-30 03:41:02 -0500

    Update version and new target for structure generation.

--- src/main/java/com/mactso/harderfarther/mixin/ChunkGenStructurePlacementMixin.java
+++ src/main/java/com/mactso/harderfarther/mixin/ChunkGenStructurePlacementMixin.java
@@ -11,10 +11,13 @@ import com.mojang.datafixers.util.Pair;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.structure.StructureManager;
 import net.minecraft.structure.StructurePlacement;
+import net.minecraft.structure.StructureTemplateManager;
 import net.minecraft.util.Holder;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.math.ChunkSectionPos;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.registry.DynamicRegistryManager;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.StructureWorldAccess;
 import net.minecraft.world.World;
@@ -22,8 +25,10 @@ import net.minecraft.world.WorldView;
 import net.minecraft.world.biome.source.BiomeCoords;
 import net.minecraft.world.biome.source.BiomeSource;
 import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.gen.RandomState;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
 import net.minecraft.world.gen.feature.StructureFeature;
+import net.minecraft.world.gen.structure.StructureSet;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -40,7 +45,9 @@ import java.util.function.Consumer;
 @Mixin(ChunkGenerator.class)
 public abstract class ChunkGenStructurePlacementMixin {
 
-    private boolean shouldgen;
+    @Shadow public abstract BiomeSource getBiomeSource();
+
+    //private boolean shouldgen;
 
     private boolean areListInitialized = false;
 
@@ -48,10 +55,10 @@ public abstract class ChunkGenStructurePlacementMixin {
     private ArrayList<List<String>> difficultySectionStructure = new ArrayList<>();
 
 
-    private static boolean areListInitializedStatic = false;
+    //private static boolean areListInitializedStatic = false;
 
-    private static ArrayList<Float> difficultySectionNumbersStatic = new ArrayList<>();
-    private static ArrayList<List<String>> difficultySectionStructureStatic = new ArrayList<>();
+    //private static ArrayList<Float> difficultySectionNumbersStatic = new ArrayList<>();
+    //private static ArrayList<List<String>> difficultySectionStructureStatic = new ArrayList<>();
 
     /*@Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/gen/structure/StructureSet;placement()Lnet/minecraft/structure/StructurePlacement;"), method = "Lnet/minecraft/world/gen/chunk/ChunkGenerator;m_mzeyuzcs(Lnet/minecraft/util/Holder;Lnet/minecraft/world/gen/RandomState;JIII)Z", cancellable = true)
     private void onGenerate(Holder<StructureSet> holder, RandomState randomState, long l, int i, int j, int k, CallbackInfoReturnable<Boolean> cir) {
@@ -122,7 +129,7 @@ public abstract class ChunkGenStructurePlacementMixin {
 
 
     //This prevents non-generated structures being located on explorer maps or with the locate command. Something interesting is that cartographers will try to locate a structure as soon as they unlock a purchasable map. That structure will be used for the map if bought.
-    @Inject(at = @At(value = "RETURN"), method = "m_gxxzcexz", cancellable = true)
+    /*@Inject(at = @At(value = "RETURN"), method = "m_gxxzcexz", cancellable = true)
     private static void harderfarther$RemovePotentialStructureResult(Set<Holder<StructureFeature>> set, WorldView world, StructureManager structureManager, boolean bl, StructurePlacement structurePlacement, ChunkPos chunkPos, CallbackInfoReturnable<@Nullable Pair<BlockPos, Holder<StructureFeature>>> cir){
         if(!areListInitializedStatic) {
 
@@ -190,16 +197,86 @@ public abstract class ChunkGenStructurePlacementMixin {
         }
 
 
+    }*/
+
+
+
+
+
+
+
+
+    //This seems to be the best target for structures. The locate structure command respects it as well.
+    @Inject(at = @At(value = "HEAD"), method = "m_dbfhxbvh", cancellable = true)
+    private void harderfarther$skipStructureGeneration(StructureSet.StructureSelectionEntry structureSelectionEntry, StructureManager structureManager, DynamicRegistryManager registryManager, RandomState randomState, StructureTemplateManager structureTemplateManager, long l, Chunk chunk, ChunkPos chunkPos, ChunkSectionPos chunkSectionPos, CallbackInfoReturnable<Boolean> cir){
+        if(!areListInitialized) {
+            synchronized (this) {
+                if (!areListInitialized) {
+
+                    StructureConfig.getDifficultySections().forEach(section -> {
+                        difficultySectionNumbers.add(section.getLeft());
+                        difficultySectionStructure.add(section.getRight());
+                    });
+
+
+                    areListInitialized = true;
+                }
+            }
+        }
+        //end of listInitialization
+
+
+
+
+        ServerWorld worldReal = ((IExtendedBiomeSourceHF)this.getBiomeSource()).getWorld();
+
+
+        int x = BiomeCoords.fromChunk(chunkPos.getCenterX());
+        int z = BiomeCoords.fromChunk(chunkPos.getCenterZ());
+
+
+        String structure = "";
+        Registry<StructureFeature> registry = worldReal.getRegistryManager().get(Registry.STRUCTURE_WORLDGEN);
+        StructureFeature structureFeature = structureSelectionEntry.structure().value();
+        structure = (String)registry.getKey(structureFeature).map(Object::toString).orElseGet(structureFeature::toString);
+        structure = structure.substring(0, structure.length() - 1);
+        structure = structure.split("/")[2].substring(1);
+
+        //System.out.println(structure);
+
+        //only overworld right now.
+        if (worldReal.getRegistryKey() == World.OVERWORLD) {
+
+
+            float difficulty = DifficultyCalculator.getDistanceDifficultyHere(worldReal, new Vec3d(x, 0, z)) * 100;
+            //System.out.println(difficulty + " is difficulty");
+
+            int[] choosenAreaIndex = {-1};
+            difficultySectionNumbers.forEach(difficultySectionNumber -> {
+                //System.out.println("Difficulty section number is " + difficultySectionNumber);
+                if (difficulty >= difficultySectionNumber) choosenAreaIndex[0]++;
+            });
+            //System.out.println(choosenAreaIndex[0] + " is Chosen index");
+
+
+            if (difficultySectionStructure.get(choosenAreaIndex[0]).get(0).equals("")) {
+                return;
+            }
+
+            if (!difficultySectionStructure.get(choosenAreaIndex[0]).contains(structure)) {
+                if(PrimaryConfig.getDebugLevel() > 0){
+                    Utility.debugMsg(1, "Canceled structure: " + structure);
+                }
+                cir.setReturnValue(false);
+            }
+        }
+
+
     }
 
 
-
-
-
-
-
     //this cancels generation, but it will still locate with the locate command or explorer map. The structure is still technically there, but structure doesn't build itself
-    @WrapWithCondition(at = @At(value = "INVOKE", target = "Ljava/util/List;forEach(Ljava/util/function/Consumer;)V"), method = "generateFeatures")
+   /* @WrapWithCondition(at = @At(value = "INVOKE", target = "Ljava/util/List;forEach(Ljava/util/function/Consumer;)V"), method = "generateFeatures")
     private boolean harderfarther$skipGenerationIfStructureDisabled(List instance, Consumer consumer){
         return this.shouldgen;
     }
@@ -315,7 +392,7 @@ public abstract class ChunkGenStructurePlacementMixin {
     @Shadow
     public BiomeSource getBiomeSource(){
         return null;
-    }
+    }*/
 
 
 }
